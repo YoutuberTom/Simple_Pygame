9230195830e9299ca9aa2af2804389ad58070e73
YoutuberTom <109851449+YoutuberTom@users.noreply.github.com>
2023-03-30 22:37:27 +0700

    Update example

--- examples/music.py
+++ examples/music.py
@@ -1,20 +1,21 @@
 """
 Play a file contains audio using the `Music` class.
 """
-import simple_pygame, os
-simple_pygame.init()
+import simple_pygame, os, sys
+successfully_imported = simple_pygame.mixer.init()
 
-path = __file__[:-len(os.path.basename(__file__))]
-file_path = os.path.join(path, "data", "Sound.mp3")
+if simple_pygame.MusicClass not in successfully_imported:
+    raise SystemExit("Import failed.")
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        path = sys.argv[0][:-len(os.path.basename(sys.argv[0]))]
+        file_path = os.path.join(path, "data", "Sound.mp3")
+    else:
+        file_path = sys.argv[1]
+
     music = simple_pygame.mixer.Music(file_path)
     music.play()
-
-    while music.get_busy():
-        pass
-
-    exception = music.get_exception()
-
-    if exception:
-        raise exception
+    music.join()
+    
+    simple_pygame.mixer.quit()
