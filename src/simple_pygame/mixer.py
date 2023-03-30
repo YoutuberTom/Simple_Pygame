d400409e48c43878dbb66ca436438dd22f591c81
YoutuberTom <109851449+YoutuberTom@users.noreply.github.com>
2023-03-30 21:45:37 +0700

    Add new functions

--- src/simple_pygame/mixer.py
+++ src/simple_pygame/mixer.py
@@ -1,10 +1,74 @@
-"""A module for playing sounds and music."""
-try:
-    from .sound import Sound
-except ImportError:
-    pass
+"""
+A module for playing sounds and music.
+"""
+__init = False
 
-try:
-    from .music import Music
-except ImportError:
-    pass
+def get_init() -> bool:
+    """
+    Return `True` if the mixer module is currently initialized, otherwise `False`.
+    """
+    return __init
+
+def init() -> tuple:
+    """
+    Initialize the mixer module and return successfully initialized classes.
+    """
+    global __init
+
+    if get_init():
+        return ()
+    __init = True
+
+    import simple_pygame
+    
+    successfully_imported = []
+
+    try:
+        global Sound
+        from .sound import Sound
+        successfully_imported.append(simple_pygame.SoundClass)
+    except ImportError:
+        pass
+
+    try:
+        global Music
+        from .music import Music
+        successfully_imported.append(simple_pygame.MusicClass)
+    except ImportError:
+        pass
+
+    if len(successfully_imported) == 0:
+        __init = False
+
+    return (*successfully_imported,)
+
+def quit() -> tuple:
+    """
+    Uninitialize the mixer module and return successfully uninitialized classes.
+    """
+    global __init
+
+    if not get_init:
+        return ()
+    __init = False
+
+    import simple_pygame, gc
+
+    successfully_quit = []
+
+    try:
+        global Sound
+        del Sound
+        successfully_quit.append(simple_pygame.SoundClass)
+    except NameError:
+        pass
+
+    try:
+        global Music
+        del Music
+        successfully_quit.append(simple_pygame.MusicClass)
+    except NameError:
+        pass
+
+    gc.collect()
+    return (*successfully_quit,)
