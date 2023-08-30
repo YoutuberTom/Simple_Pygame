b85bfb64672cee4d7c1f82cbc0a495efd9c298b3
YoutuberTom <109851449+YoutuberTom@users.noreply.github.com>
2023-08-30 22:41:48 +0700

    Improved the code
    Improvements:
    - Updated to newly added exceptions
    - Fixed an error related to not having ffmpeg/ffprobe

--- tests/audio_test.py
+++ tests/audio_test.py
@@ -35,19 +35,19 @@ class TestAudio(unittest.TestCase):
 
         try:
             self.assertEqual(type(self.audio.get_information(self.audio.path)), dict, "The return value must be a dict.")
-        except FileNotFoundError:
+        except simple_pygame.FFprobeError:
             self.skipTest("No ffprobe found.")
 
         try:
             self.assertEqual(type(self.audio.get_information(self.audio.path, encoding = "encoding")), dict, "The return value must be a dict.")
-        except FileNotFoundError:
+        except simple_pygame.FFprobeError:
             self.skipTest("No ffprobe found.")
         except ValueError:
             pass
 
         try:
             self.assertEqual(type(self.audio.get_information(self.audio.path, "utf-8", True, "ffmpeg")), dict, "The return value must be a dict.")
-        except FileNotFoundError:
+        except simple_pygame.FFmpegError:
             self.skipTest("No ffmpeg found.")
 
     def test_create_pipe(self) -> None:
@@ -56,8 +56,10 @@ class TestAudio(unittest.TestCase):
 
         try:
             pipe, information, stream_information = self.audio.create_pipe(self.audio.path, stream = -1)
-        except FileNotFoundError:
+        except simple_pygame.FFmpegError:
             self.skipTest("No ffmpeg found.")
+        except simple_pygame.FFprobeError:
+            self.skipTest("No ffprobe found.")
 
         self.assertEqual(type(pipe), subprocess.Popen, "Pipe must be a subprocess.Popen object.")
         self.assertEqual(type(information), dict, "Information must be a dict.")
@@ -122,7 +124,15 @@ class TestAudio(unittest.TestCase):
         self.assertTrue(self.audio.get_busy(), "Play audio failed.")
         self.assertEqual(self.audio.get_position(), simple_pygame.AudioIsLoading, "Invalid audio position.")
 
-        time.sleep(0.1)
+        while self.audio.get_position() == simple_pygame.AudioIsLoading:
+            time.sleep(0.1)
+
+        try:
+            self.audio.get_exception()
+        except simple_pygame.FFmpegError:
+            self.skipTest("No ffmpeg found.")
+        except simple_pygame.FFprobeError:
+            self.skipTest("No ffprobe found.")
 
         self.audio.pause()
         self.assertTrue(self.audio.get_pause(), "Pause audio failed.")
@@ -163,6 +173,16 @@ class TestAudio(unittest.TestCase):
         self.assertTrue(self.audio.get_busy(), "Play audio failed.")
         self.assertEqual(self.audio.get_position(), simple_pygame.AudioIsLoading, "Invalid audio position.")
 
+        while self.audio.get_position() == simple_pygame.AudioIsLoading:
+            time.sleep(0.1)
+
+        try:
+            self.audio.get_exception()
+        except simple_pygame.FFmpegError:
+            self.skipTest("No ffmpeg found.")
+        except simple_pygame.FFprobeError:
+            self.skipTest("No ffprobe found.")
+
         self.audio.join()
         self.assertFalse(self.audio.get_busy(), "Join audio failed..")
         self.assertEqual(self.audio.get_position(), simple_pygame.AudioEnded, "Invalid audio position.")
@@ -187,7 +207,7 @@ class TestAudio(unittest.TestCase):
         while self.audio.get_position() != simple_pygame.AudioEnded:
             time.sleep(0.1)
 
-        with self.assertRaises(ValueError, msg = "Expected ValueError."):
+        with self.assertRaises(simple_pygame.FFprobeError, msg = "Expected FFprobeError."):
             self.audio.get_exception()
 
         self.audio.path = self.file_path
