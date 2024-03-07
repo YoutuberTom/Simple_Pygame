12edce282e47cffbfa2d701ea0b800ecb8610671
YoutuberTom <109851449+YoutuberTom@users.noreply.github.com>
2024-03-07 23:38:17 +0700

    Rename some variables to `_<variable name>`

--- src/simple_pygame/terminal/pygetch.py
+++ src/simple_pygame/terminal/pygetch.py
@@ -1,36 +1,37 @@
 """
 A module for working with sys.stdin on Unix.
 """
-import termios, tty, select, sys
+import termios as _termios, tty as _tty, sys as _sys
+from select import select
 
-_original_tty_attributes = termios.tcgetattr(sys.stdin)
+_original_tty_attributes = _termios.tcgetattr(_sys.stdin)
 
 def setcooked() -> None:
     """
     Change the mode of sys.stdin to what it was when this module was imported.
     """
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, _original_tty_attributes)
+    _termios.tcsetattr(_sys.stdin, _termios.TCSADRAIN, _original_tty_attributes)
 
 def setraw() -> None:
     """
     Change the mode of sys.stdin to raw.
     """
-    tty.setraw(sys.stdin)
+    _tty.setraw(_sys.stdin)
 
 def setcbreak() -> None:
     """
     Change the mode of sys.stdin to cbreak.
     """
-    tty.setcbreak(sys.stdin)
+    _tty.setcbreak(_sys.stdin)
 
 def getch() -> str:
     """
     Return a character read from sys.stdin.
     """
-    return sys.stdin.read(1)
+    return _sys.stdin.read(1)
 
 def kbhit() -> bool:
     """
     Return `True` if a new keypress is detected on sys.stdin. After any character has been read, `False` will be returned instead.
     """
-    return sys.stdin in select.select((sys.stdin,), (), (), 0)[0]
+    return _sys.stdin in select((_sys.stdin,), (), (), 0)[0]
